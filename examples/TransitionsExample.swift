1ef994decb7578a08e6e9c581f8a108c8eb758af
Jeff Verkoeyen <featherless@google.com>
2017-04-24 17:18:47 -0400

    Add a switch for toggling whether the presented view controller should be presented over the current context.
    Reviewers: O2 Material Motion, O4 Material Apple platform reviewers, #material_motion
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D3101

--- examples/TransitionsExample.swift
+++ examples/TransitionsExample.swift
@@ -33,6 +33,7 @@ class TransitionsExampleViewController: ExampleViewController {
   }
 
   var picker: UIPickerView!
+  var sizeSwitch: UISwitch!
   override func viewDidLoad() {
     super.viewDidLoad()
 
@@ -43,11 +44,20 @@ class TransitionsExampleViewController: ExampleViewController {
     picker.dataSource = self
     picker.delegate = self
     view.addSubview(picker)
+
+    sizeSwitch = UISwitch()
+    let sizeSize = sizeSwitch.sizeThatFits(view.bounds.size)
+    sizeSwitch.frame = .init(origin: .init(x: view.bounds.midX - sizeSize.width / 2, y: picker.frame.maxY), size: size)
+    view.addSubview(sizeSwitch)
   }
 
   func didTap() {
     let vc = ModalViewController()
     vc.transitionController.transitionType = transitions[picker.selectedRow(inComponent: 0)]
+    if sizeSwitch.isOn {
+      vc.preferredContentSize = .init(width: 100, height: 100)
+      vc.modalPresentationStyle = .overCurrentContext
+    }
     present(vc, animated: true)
   }
 
